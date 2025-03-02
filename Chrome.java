70a47578a412482845dd26b3d0f8ebb8c960d15a
Umamaheswari1907 <umamaheswari1907@gmail.com>
2025-03-02 13:54:01 +0530

    Java Data Types

---
+++ Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float versionNo = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseyear = 2008;
+		char shortcutkey = 'C';
+		System.out.println("Chrome Browser Details:");
+		System.out.println("Brower Version No:" +versionNo + " and " + "Developed By:" +developer);
+		System.out.println("Is Beta Version:" +isBeta + " and " + "Release Year:" +releaseyear);
+		System.out.println("Brower Shortcut Key:" +shortcutkey);
+
+	}
+
+}
+
---
+++ Firefox.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float browserVersion= 100.2f;
+String browserName =  "firefox";
+boolean isVisible = true;		
+int releaseYear = 1998;
+char browserLogo = 'f';
+System.out.println("Data Types examples are here:");
+System.out.println("Brower Name:" +browserName + "; " + "Browser Version:" +browserVersion);
+System.out.println("Is Visible:" +isVisible + "; " + "Release Year:" +releaseYear);
+System.out.println("Brower Logo:" +browserLogo);
+
+	}
+
+}
+
